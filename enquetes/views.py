7d8f75c1b75b7edfcb59752584ed2cf800d21f17
Giovana Barros <giovana.b@escolar.ifrn.edu.br>
2024-03-18 17:11:31 +0000

    Atividade

--- enquetes/views.py
+++ enquetes/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def  index (request):
-    return HttpResponse('DSWeb - 2024.1<br>Matrícula: 20231014040030<br>Nome: Giovana Barros ')
+    return HttpResponse('DSWeb - 2024.1<br>3º período<br>Matrícula: 20231014040030<br>Nome: Giovana Barros ')
