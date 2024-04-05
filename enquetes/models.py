1c447140f1541bb98d42a226616eb92c9652e02f
Giovana Barros <giovana.b@escolar.ifrn.edu.br>
2024-04-05 12:39:26 +0000

    atividade

--- enquetes/models.py
+++ enquetes/models.py
@@ -2,10 +2,9 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 class Pergunta(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('Data de publicação')
+    texto = models.CharField(max_length=150)
+    data_pub = models.DateTimeField('Data de publicação')
     def __str__(self):
         return '{} ({})'.format(self.texto, self.id)
     def publicada_recentemente(self):
@@ -13,8 +12,10 @@ class Pergunta(models.Model):
         return self.data_pub >= agora - datetime.timedelta(hours=24)
 
 class Alternativa(models.Model):
-    question = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    texto = models.CharField(max_length=100)
+    quant_votos = models.IntegerField(
+        'Quantidade de votos', default=0
+    )
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
     def __str__(self):
         return '{} ({})'.format(self.texto, self.id)
--- enquetes/urls.py
+++ enquetes/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path ('', views.index, name = 'index'),
+    path('', views.index, name = 'index'),
+    path('<int:pergunta_id>/', views.detalhes, name='detalhes'),
+    path('<int:pergunta_id>/votacao/', views.votacao, name='votacao'),
+    path('<int:pergunta_id>/resultado/', views.resultado, name='resultado'),
 ]
